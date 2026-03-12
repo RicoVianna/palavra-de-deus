56fcc02aca890d7338b56f684bd240e9d5f25f9e
RicoVianna <rluivianna@hotmail.com>
2026-03-12 18:43:13 -0300

    Atualização final: manifest, servic-worker e CSS corrigidos

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 self.addEventListener("install", function(event) {
   console.log("Service Worker instalado");
 });
 
 self.addEventListener("fetch", function(event) {
-=======
-self.addEventListener("install", function(event) {
-  console.log("Service Worker instalado");
-});
 
-self.addEventListener("fetch", function(event) {
->>>>>>> d83c774428df168056ffe70a59bcd2793518839d
 });
